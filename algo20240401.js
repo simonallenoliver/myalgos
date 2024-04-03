b0c75a8279dbba384a6e0bd1c6f2313a36c7ee60
SimonAllenOliver <simonallenoliver@gmail.com>
2024-04-03 18:33:05 -0500

    new algo

--- algo20240401.js
+++ algo20240401.js
@@ -2,3 +2,22 @@
 // toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
 // notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".
 
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
+        }
+    }
+};
+
+console.log(expect(5).toBe(5))
+console.log(expect(5).notToBe(5))
