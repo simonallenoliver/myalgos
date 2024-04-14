2802e3e8328a10f9e5a50c84cc5ecde6df1bf667
SimonAllenOliver <simonallenoliver@gmail.com>
2024-04-13 20:11:15 -0500

    new algo

--- algo20240412.js
+++ algo20240412.js
@@ -4,3 +4,21 @@
 // Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, 
 // and an integer n, return true if n new flowers can be planted in the flowerbed without violating 
 // the no-adjacent-flowers rule and false otherwise.
+
+// my solution
+
+var canPlaceFlowers = function(flowerbed, n) {
+    for(let i = 0; i < flowerbed.length; i ++)
+    if(flowerbed[i] === 0){
+        if((flowerbed[i+1] === 0 && flowerbed[i-1] === 0) || 
+        (flowerbed[i+1] === 0 && flowerbed[i-1] === undefined) ||
+        (flowerbed[i+1] === undefined && flowerbed[i-1] === 0) ||
+        (flowerbed[i+1] === undefined && flowerbed[i-1] === undefined)){
+            flowerbed[i] = 1
+            n -= 1
+        }
+    }
+
+    if(n > 0){return false}
+    else{return true}
+};
