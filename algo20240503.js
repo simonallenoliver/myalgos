80330fd6fc880ef94c02593cc0dbe1375dfbf6a5
SimonAllenOliver <simonallenoliver@gmail.com>
2024-05-03 12:55:39 -0500

    longest subArray after deletion

---
+++ algo20240503.js
@@ -0,0 +1,7 @@
+// Given a binary array nums, you should delete one element from it.
+
+// Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+
+var longestSubarray = function(nums) {
+    
+};
