d48ae651a23fa8600ff53f4487455d804225d2df
SimonAllenOliver <simonallenoliver@gmail.com>
2024-05-06 17:21:26 -0500

    algo update

--- algo20240503.js
+++ algo20240503.js
@@ -2,6 +2,23 @@
 
 // Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
 
-var longestSubarray = function(nums) {
-    
-};
+
+var longestSubarray = function (nums) {
+    let left = 0;
+    let right = 0;
+    let max = 0;
+    let deleted = false
+
+    while (right < nums.length) {
+        if (deleted = true) {
+            if (nums[right === 1]) {
+                right++
+
+            }
+            else {
+                deleted = true
+            }
+        }
+
+    }
+}
