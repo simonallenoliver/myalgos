2002fa26ccd9f48dc567f2420214e1a57a8ace3f
SimonAllenOliver <simonallenoliver@gmail.com>
2024-07-02 20:09:44 -0500

    flowers algo

--- algo20240627.js
+++ algo20240627.js
@@ -7,3 +7,18 @@
 // no-adjacent-flowers rule and false otherwise.
 
 
+var canPlaceFlowers = function(flowerbed, n) {
+    // iterate through the flowerbed
+    for(let i = 0; i < flowerbed.length; i++){
+        // check to see if each spots adjacent spots are full
+        if(flowerbed[i] === 0 && (flowerbed[i-1] === 0 || flowerbed[i-1] === undefined) && (flowerbed[i+1] === 0 || flowerbed[i+1] === undefined)){
+            // if not full, add a flower (1) in that spot and decrement n (our stock of flowers)
+            flowerbed[i] = 1;
+            n--;
+        }
+    }
+    // after our loop, if n is greater than 0, it means there wasn't 
+    // enough room for all the flowers in our stock n
+    if(n > 0) return false;
+    return true
+};
