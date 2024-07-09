71abbbd22a790c006c2a75aff8b7a43b050574e1
SimonAllenOliver <simonallenoliver@gmail.com>
2024-07-08 19:58:22 -0500

    partial

--- algo20240703.js
+++ algo20240703.js
@@ -1,2 +1,19 @@
 // Given a string s, reverse only all the vowels in the string and return it.
 // The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+var reverseVowels = function(s) {
+    let newVows = []
+    const vowels = "aeiouAEIOU"
+    for(let i = 0; i < s.length; i++){
+        if(vowels.includes(s[i])){
+            newVows.push(s[i])
+        }
+    }
+    console.log('NW', newVows)
+    for(let i = 0; i < s.length; i++){
+        if(vowels.includes(s[i])){
+            s[i] = 3
+        }
+    }
+    return s
+};
